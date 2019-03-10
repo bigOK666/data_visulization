52aed5cf95788cee647014cd3ab6a05a3d173fe0
Lu <zeren.lu@gmail.com>
2019-03-10 20:52:56 +0100

    use matplotlib to plot a bar chart with specific fontsizzi

--- data_visulization.py
+++ data_visulization.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('Topic_Survey_Assignment.csv', index_col=0)
 
@@ -12,3 +13,12 @@ df_decend = df.sort_values(by='Very interested', ascending=False)
 survey_sum = 2233
 
 df_decend.loc[:, 'Very interested':'Not interested'] = df_decend.loc[:, 'Very interested':'Not interested'].div(survey_sum, axis=0).round(4).mul(100, axis=0)
+# draw the bar charts
+fig, ax = plt.subplots(figsize=(20,8))
+bar_width=0.8
+color_very_interested='#5cb85c'
+color_somewhat_interested='#5bc0de'
+color_not_interested='#d9534f'
+df_decend.plot(kind='bar',ax = ax, title="Percentage of Respondents' Interest in Data Science Areas", color=[color_very_interested, color_somewhat_interested, color_not_interested], width=bar_width, fontsize=14)
+ax.title.set_size(16)
+plt.legend(fontsize=14)
