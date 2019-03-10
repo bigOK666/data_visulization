259c7aad398d4377e4bb5147632798dd0dc92cad
Lu <zeren.lu@gmail.com>
2019-03-10 16:37:47 +0100

    sort the data based decending "Very interested"

--- data_visulization.py
+++ data_visulization.py
@@ -3,3 +3,7 @@ import pandas as pd
 df = pd.read_csv('Topic_Survey_Assignment.csv', index_col=0)
 
 print(df.head())
+
+#sort values based on "very interested" in decending method
+
+df.sort_values(by='Very interested', ascending=False)
