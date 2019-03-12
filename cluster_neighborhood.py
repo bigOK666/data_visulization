64399ed23857ab0af71158a6aa0d0a87149e6741
Lu <zeren.lu@gmail.com>
2019-03-12 22:58:11 +0100

    use soup to get the table html content

--- cluster_neighborhood.py
+++ cluster_neighborhood.py
@@ -4,4 +4,9 @@ from bs4 import BeautifulSoup
 # get the content of wikipedia page
 website_url = requests.get('https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M').text
 soup=BeautifulSoup(website_url, 'lxml')
-print(soup.prettify())
+#print(soup.prettify())
+
+### since the table data is in table tag with class wikitable sortable so we can get them
+
+My_table=soup.find('table', {'class':'wikitable sortable'})
+print(My_table)
