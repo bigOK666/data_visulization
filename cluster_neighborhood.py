6931269bf39109740cb19f78020c6d02f75843eb
Lu <zeren.lu@gmail.com>
2019-03-12 23:33:31 +0100

    create first version of dataframe from webpage data

--- cluster_neighborhood.py
+++ cluster_neighborhood.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 # get the content of wikipedia page
 website_url = requests.get('https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M').text
@@ -20,3 +21,7 @@ print(headers)
 rows = []
 for row in My_table.find_all('tr'):
     rows.append([val.text for val in row.find_all('td')])
+
+### create raw dataframe from webpage
+df = pd.DataFrame(rows, columns = headers)
+df.head()
